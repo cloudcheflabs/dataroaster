56323e246cb20c9b641382a3b55231f4c625aee3
Kidong Lee <mykidong@gmail.com>
2022-06-11 15:08:43 +0900

    trino proxy test runner added...

--- trino-ecosystem/trino-gateway/src/test/java/com/cloudcheflabs/dataroaster/trino/gateway/TrinoProxyTestRunner.java
+++ trino-ecosystem/trino-gateway/src/test/java/com/cloudcheflabs/dataroaster/trino/gateway/TrinoProxyTestRunner.java
@@ -1,5 +1,6 @@
-package com.cloudcheflabs.dataroaster.trino.gateway.test;
+package com.cloudcheflabs.dataroaster.trino.gateway;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,5 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(properties = {
         "spring.config.location=classpath:application-test.properties"
 })
-public class SpringTestBase {
+public class TrinoProxyTestRunner {
+
+    @Test
+    public void runTrinoProxy() throws Exception {
+        Thread.sleep(Long.MAX_VALUE);
+    }
 }
