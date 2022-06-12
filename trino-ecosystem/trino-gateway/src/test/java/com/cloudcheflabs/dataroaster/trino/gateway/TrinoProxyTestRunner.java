709a5b39bec6c93d17dfdee7fa0403a389295118
Kidong Lee <mykidong@gmail.com>
2022-06-13 02:39:24 +0900

    .

--- trino-ecosystem/trino-gateway/src/test/java/com/cloudcheflabs/dataroaster/trino/gateway/TrinoProxyTestRunner.java
+++ trino-ecosystem/trino-gateway/src/test/java/com/cloudcheflabs/dataroaster/trino/gateway/TrinoProxyTestRunner.java
@@ -1,22 +1,18 @@
 package com.cloudcheflabs.dataroaster.trino.gateway;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.SpringApplication;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = TrinoGatewayApplication.class)
-@TestPropertySource(locations = "classpath:application-test.properties")
-@AutoConfigureMockMvc
+import java.util.Arrays;
+
 public class TrinoProxyTestRunner {
 
-    @Autowired
-    private MockMvc mvc;
+    @BeforeClass
+    public static void setup() throws Exception {
+        SpringApplication.run(TrinoGatewayApplication.class, Arrays.asList("").toArray(new String[0]));
+    }
+
 
     @Test
     public void run() throws Exception {
