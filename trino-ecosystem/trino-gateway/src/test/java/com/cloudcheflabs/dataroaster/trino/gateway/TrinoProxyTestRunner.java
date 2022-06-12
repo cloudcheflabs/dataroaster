03fa0236d86a54dd0e5b73110e224291d36b09f3
Kidong Lee <mykidong@gmail.com>
2022-06-13 01:38:28 +0900

    .

--- trino-ecosystem/trino-gateway/src/test/java/com/cloudcheflabs/dataroaster/trino/gateway/TrinoProxyTestRunner.java
+++ trino-ecosystem/trino-gateway/src/test/java/com/cloudcheflabs/dataroaster/trino/gateway/TrinoProxyTestRunner.java
@@ -1,15 +1,22 @@
 package com.cloudcheflabs.dataroaster.trino.gateway;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TrinoGatewayApplication.class)
 @TestPropertySource(locations = "classpath:application-test.properties")
+@AutoConfigureMockMvc
 public class TrinoProxyTestRunner {
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     public void runTrinoProxy() throws Exception {
