4cb0b31eeb08da4d47e1ec406e0918f9d98a661e
Kidong Lee <mykidong@gmail.com>
2022-06-22 13:43:00 +0900

    admin user creation with randomly generated password...

--- operators/dataroaster/dataroaster-operator/src/main/java/com/cloudcheflabs/dataroaster/operators/dataroaster/util/RandomUtils.java
+++ operators/dataroaster/dataroaster-operator/src/main/java/com/cloudcheflabs/dataroaster/operators/dataroaster/util/RandomUtils.java
@@ -1,6 +1,7 @@
 package com.cloudcheflabs.dataroaster.operators.dataroaster.util;
 
 import java.nio.charset.Charset;
+import java.util.Base64;
 import java.util.Random;
 
 public class RandomUtils {
@@ -9,5 +10,12 @@ public class RandomUtils {
         byte[] array = new byte[20];
         new Random().nextBytes(array);
         return new String(array, Charset.forName("UTF-8"));
+    }
+
+    public static String randomPassword() {
+        String randomText = randomText();
+        String encodedString = Base64.getEncoder().encodeToString(randomText);
+
+        return encodedString;
     }
 }
