f5ec34e774faae4bac787e5d25c4139ece06f5a7
Kidong Lee <mykidong@gmail.com>
2022-06-22 11:58:59 +0900

    mysql chart for dataroaster added...

--- operators/dataroaster/dataroaster-operator/src/main/java/com/cloudcheflabs/dataroaster/operators/dataroaster/dao/kubernetes/KubernetesK8sResourceDao.java
+++ operators/dataroaster/dataroaster-operator/src/main/java/com/cloudcheflabs/dataroaster/operators/dataroaster/dao/kubernetes/KubernetesK8sResourceDao.java
@@ -3,8 +3,12 @@ package com.cloudcheflabs.dataroaster.operators.dataroaster.dao.kubernetes;
 import com.cloudcheflabs.dataroaster.operators.dataroaster.api.dao.K8sResourceDao;
 import com.cloudcheflabs.dataroaster.operators.dataroaster.dao.common.AbstractKubernetesDao;
 import com.cloudcheflabs.dataroaster.operators.dataroaster.domain.model.CustomResource;
+import com.cloudcheflabs.dataroaster.operators.dataroaster.service.UserTokenServiceImpl;
 import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
 import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinitionList;
+import jdk.internal.org.jline.utils.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,19 +18,36 @@ import java.util.List;
 @Transactional
 public class KubernetesK8sResourceDao extends AbstractKubernetesDao implements K8sResourceDao {
 
+    private static Logger LOG = LoggerFactory.getLogger(KubernetesK8sResourceDao.class);
+
     @Override
     public void createCustomResource(CustomResource customResource) {
-        CustomResourceDefinitionList crds = kubernetesClient.apiextensions().v1().customResourceDefinitions().list();
-        List<CustomResourceDefinition> crdsItems = crds.getItems();
+        try {
+            CustomResourceDefinitionList crds = kubernetesClient.apiextensions().v1().customResourceDefinitions().list();
+            List<CustomResourceDefinition> crdsItems = crds.getItems();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void deleteCustomResource(String name, String namespace) {
-
+        try {
+            // TODO
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void updateCustomResource(CustomResource customResource) {
-
+        try {
+            // TODO
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
