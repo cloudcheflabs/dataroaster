2521c0cddcffc15f092761a86fb6934891e5a074
Kidong Lee <mykidong@gmail.com>
2022-06-24 15:10:11 +0900

    prop added...

--- operators/dataroaster/dataroaster-operator/src/main/java/com/cloudcheflabs/dataroaster/operators/dataroaster/DataRoasterApplication.java
+++ operators/dataroaster/dataroaster-operator/src/main/java/com/cloudcheflabs/dataroaster/operators/dataroaster/DataRoasterApplication.java
@@ -1,5 +1,6 @@
 package com.cloudcheflabs.dataroaster.operators.dataroaster;
 
+import com.cloudcheflabs.dataroaster.operators.dataroaster.component.DBSchemaCreator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -18,7 +19,13 @@ public class DataRoasterApplication {
 	private static Logger LOG = LoggerFactory.getLogger(DataRoasterApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataRoasterApplication.class, args);
+		String createMySQLDBSchema = System.getProperty("createMySQLDBSchema");
+		if(createMySQLDBSchema != null) {
+			LOG.info("createMySQLDBSchema: [{}]", createMySQLDBSchema);
+			DBSchemaCreator.main(args);
+		} else {
+			SpringApplication.run(DataRoasterApplication.class, args);
+		}
 	}
 
 	@Bean
