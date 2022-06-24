2842753663f512d7f23a516a7ff363c43711a4dd
Kidong Lee <mykidong@gmail.com>
2022-06-24 18:41:41 +0900

    .

--- operators/dataroaster/dataroaster-operator/src/test/java/com/cloudcheflabs/dataroaster/operators/dataroaster/component/DBSchemaCreatorTestRunner.java
+++ operators/dataroaster/dataroaster-operator/src/test/java/com/cloudcheflabs/dataroaster/operators/dataroaster/component/DBSchemaCreatorTestRunner.java
@@ -1,14 +1,33 @@
 package com.cloudcheflabs.dataroaster.operators.dataroaster.component;
 
+import org.apache.ibatis.jdbc.ScriptRunner;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Arrays;
 
 public class DBSchemaCreatorTestRunner {
 
     @Test
     public void createSchema() throws Exception {
-        // system property -DdataroasterKubeconfig=... must be set before running test.
-        DBSchemaCreator.main(Arrays.asList("host", "root", "mysqlpass123", "/opt/dataroaster-operator/create-tables.sql").toArray(new String[0]));
+        //Registering the Driver
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+
+        //Getting the connection
+        String mysqlUrl = "jdbc:mysql://localhost:3306/dataroaster?createDatabaseIfNotExist=true&useSSL=false";
+
+        Connection con = DriverManager.getConnection(mysqlUrl, "root", "mysqlpass123");
+        System.out.println("Connection established......");
+        
+        //Initialize the script runner
+        ScriptRunner sr = new ScriptRunner(con);
+        //Creating a reader object
+        Reader reader = new BufferedReader(new FileReader("/opt/dataroaster-operator/create-tables.sql"));
+        //Running the script
+        sr.runScript(reader);
     }
 }
