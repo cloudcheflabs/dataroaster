ba0b4ab11f9d22f0e97146008706c7d1171ca4fb
Kidong Lee <mykidong@gmail.com>
2022-07-10 16:18:38 +0900

    .

--- trino-ecosystem/trino-gateway/src/main/java/com/cloudcheflabs/dataroaster/trino/gateway/util/GzipUtils.java
+++ trino-ecosystem/trino-gateway/src/main/java/com/cloudcheflabs/dataroaster/trino/gateway/util/GzipUtils.java
@@ -2,6 +2,7 @@ package com.cloudcheflabs.dataroaster.trino.gateway.util;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
@@ -9,50 +10,71 @@ import java.util.zip.GZIPOutputStream;
 public class GzipUtils {
 
     public static byte[] compressStringInGzip(String content) {
-        byte[] bytes;
-        try {
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            GZIPOutputStream gos = new GZIPOutputStream(os);
-
-            InputStream is = new ByteArrayInputStream(content.getBytes());
-            byte[] buffer = new byte[1024];
-            int len;
-            while((len = is.read(buffer)) != -1){
-                gos.write(buffer, 0, len);
-            }
-
-            bytes = os.toByteArray();
-
-            gos.close();
-            os.close();
-            is.close();
-
-            return bytes;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return compress(content.getBytes());
     }
 
-    public static String decompressGzip(byte[] bytes) {
-        String content;
-        try {
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
 
-            InputStream is = new ByteArrayInputStream(bytes);
-            GZIPInputStream gis = new GZIPInputStream(is);
+    private static byte[] compress(byte[] uncompressedData) {
+        ByteArrayOutputStream bos = null;
+        GZIPOutputStream gzipOS = null;
+        try {
+            bos = new ByteArrayOutputStream(uncompressedData.length);
+            gzipOS = new GZIPOutputStream(bos);
+            gzipOS.write(uncompressedData);
+            gzipOS.close();
+            return bos.toByteArray();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                assert gzipOS != null;
+                gzipOS.close();
+                bos.close();
+            }
+            catch (Exception ignored) {
+            }
+        }
+        return new byte[]{};
+    }
+
+
+    public static String decompressGzip(byte[] bytes) {
+        return new String(uncompress(bytes));
+    }
+
+
+    private static byte[] uncompress(byte[] compressedData) {
+        ByteArrayInputStream bis = null;
+        ByteArrayOutputStream bos = null;
+        GZIPInputStream gzipIS = null;
+
+        try {
+            bis = new ByteArrayInputStream(compressedData);
+            bos = new ByteArrayOutputStream();
+            gzipIS = new GZIPInputStream(bis);
 
             byte[] buffer = new byte[1024];
             int len;
-            while ((len = gis.read(buffer)) != -1) {
-                os.write(buffer, 0, len);
+            while((len = gzipIS.read(buffer)) != -1){
+                bos.write(buffer, 0, len);
             }
-            content = new String(os.toByteArray());
-            os.close();
-            gis.close();
-            is.close();
-            return content;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            return bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        finally {
+            try {
+                assert gzipIS != null;
+                gzipIS.close();
+                bos.close();
+                bis.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return new byte[]{};
     }
 }
