011003d189b1e803bcc1cdb982562bfbfe36386c
Kidong Lee <mykidong@gmail.com>
2022-07-11 22:57:14 +0900

    .

---
+++ trino-ecosystem/trino-controller/src/test/java/com/cloudcheflabs/dataroaster/trino/controller/util/TemplateUtilsTestRunner.java
@@ -0,0 +1,20 @@
+package com.cloudcheflabs.dataroaster.trino.controller.util;
+
+import com.cedarsoftware.util.io.JsonWriter;
+import com.cloudcheflabs.dataroaster.common.util.TemplateUtils;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TemplateUtilsTestRunner {
+
+    @Test
+    public void replace() throws Exception{
+        Map<String, Object> kv = new HashMap<>();
+        kv.put("customResourceNamespace", "trino-controller");
+        String nginxCrString =
+                TemplateUtils.replace("/templates/cr/nginx-ingress-controller.yaml", true, kv);
+        System.out.printf("nginxCrString: \n%s", JsonWriter.formatJson(nginxCrString));
+    }
+}
