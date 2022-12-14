5ab4418311585eed9d045cb24f5875268d9c5e14
Kidong Lee <mykidong@gmail.com>
2022-12-14 13:40:21 +0900

    spark version up to 3.2.2

--- components/hive/spark-thrift-server/src/main/java/com/cloudcheflabs/dataroaster/hive/SparkThriftServerRunner.java
+++ components/hive/spark-thrift-server/src/main/java/com/cloudcheflabs/dataroaster/hive/SparkThriftServerRunner.java
@@ -4,5 +4,13 @@ public class SparkThriftServerRunner {
 
     public static void main(String[] args) {
         org.apache.spark.sql.hive.thriftserver.HiveThriftServer2.main(args);
+
+        while (true) {
+            try {
+                Thread.sleep(Long.MAX_VALUE);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
